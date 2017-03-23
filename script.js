d73b4ca899739b1f88d488643bf1f3b400b3315e
Paulson Li <paulsonli99@gmail.com>
2017-03-23 20:21:28 +0000

    reeeeeee

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function (){
+var interbal = setInterval(function () {
+    var picTop = $("#pic").offset().top;
+    var picBottom = 
+    $("#pic").css("top", $("#pic").offset().top + 10);
+}, 1000);
+    
+    
+    
+});
