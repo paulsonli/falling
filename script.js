4860bc496d01abc979d6cfa7444366d5f6911112
Paulson Li <paulsonli99@gmail.com>
2017-03-24 21:24:41 +0000

    finish drop and rng spawn

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+/*global $*/
 $(document).ready(function (){
-var interbal = setInterval(function () {
+var RNGspawn = Math.floor((Math.random() * 1600) + 1);
+$("#pic").css("left", RNGspawn);
+console.log(RNGspawn);
+
+
+    
+var interval = setInterval(function () {
     var picTop = $("#pic").offset().top;
-    var picBottom = 
+    var picBottom = picTop + $("#pic").height();
+    var groundTop = $("#ground").offset().top;
+    var groundBottom = groundTop + $("#ground").height();
+    if (picBottom > groundTop) {
+        clearInterval(interval);
+    }
     $("#pic").css("top", $("#pic").offset().top + 10);
-}, 1000);
+}, 50);
     
     
     
